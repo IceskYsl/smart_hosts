00c50407f70e4b15722e14a668b5685c514b7b90
Iceskysl <iceskysl@gmail.com>
2013-12-13 09:04:12 +0800

    fix readme file

--- merge_tool.sh
+++ merge_tool.sh
@@ -1,5 +1,8 @@
 #!/bin/bash 
 echo "start merge *.hosts into smart..."
 cat configs/* > smart_hosts
+echo "end merge *.hosts into smart..."
+echo "***************"
 echo "start merge smart to /etc/host"
 cat smart_hosts >> /etc/hosts
+echo "end merge smart to /etc/host"
